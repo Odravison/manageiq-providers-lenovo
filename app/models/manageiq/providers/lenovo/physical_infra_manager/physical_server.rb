b6656ed3839b2ca69b26d59d6eb3d73163e7d8d1
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2017-03-20 09:17:41 -0300

    [XCS-155] Removes foreign-key flag 1. Removing foreign-key flag to hardware model of physical_server model

--- app/models/manageiq/providers/lenovo/physical_infra_manager/physical_server.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/physical_server.rb
@@ -3,6 +3,6 @@ module ManageIQ::Providers
     belongs_to :ext_management_system, foreign_key: "ems_id", class_name: "ManageIQ::Providers::Lenovo::PhysicalInfraManager"
     has_many :firmwares, :foreign_key => "ph_server_uuid", :primary_key => "uuid"
     has_one :host, :foreign_key => "service_tag", :primary_key => "serialNumber"
-    has_one :hardware, :foreign_key => "physical_server_id", :primary_key => "id"
+    has_one :hardware 
   end
 end
